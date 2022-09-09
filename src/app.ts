4c156ec039ef5868eedd8e7c3649873bf3830328
PhatPhamFE <phatphamm01@gmail.com>
2022-09-09 13:32:30 +0700

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import webPageRouter from './routes/webPage.routes';
 import AppError from './utils/AppError.utils';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
 import ExpressMongoSanitize from 'express-mongo-sanitize';
 import logger from 'morgan';
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json({ limit: '25mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(ExpressMongoSanitize());
+app.options('*', cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({
